dccb716840e3a7acea99c2f75159269e38d9e9f1
AbdulManaf12 <sahitoa97@gmail.com>
2023-06-24 00:29:18 +0500

    added edit and delete api

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ mongoose
   });
 
 const app = express();
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get("/", async (req, res) => {
@@ -26,6 +27,42 @@ app.get("/", async (req, res) => {
 app.get("/Books/:id", async (req, res) => {
   const filteredbook = await BookSchema.find({ _id: req.params.id });
   res.render("BookDetails.ejs", { book: filteredbook[0] });
+});
+
+app.get("/add-book", (req, res) => {
+  res.render("addBook.ejs");
+});
+
+app.post("/add-data", (req, res) => {
+  BookSchema.insertMany(req.body);
+  res.redirect("/");
+});
+
+app.get("/books/:id/edit", async (req, res) => {
+  const filteredbook = await BookSchema.find({ _id: req.params.id });
+  res.render("editBook.ejs", { book: filteredbook[0] });
+});
+
+app.post("/books/:id/upate-data", (req, res) => {
+  BookSchema.updateOne({ _id: req.params.id }, req.body)
+    .then(() => {
+      console.log("Updated successfully");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  res.redirect("/");
+});
+
+app.post("/books/:id/method=DELETE", async (req, res) => {
+  BookSchema.deleteOne({ _id: req.params.id })
+    .then(() => {
+      console.log("Deleted successfully");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  res.redirect("/");
 });
 
 app.get("/about", (req, res) => {
